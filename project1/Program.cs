2c951246475cea1ad034e11ca3ff1ab4f116e2bb
cerjakt <cerjakt@dunwoody.edu>
2020-02-18 11:55:55 -0600

    Added a player class

--- project1/Program.cs
+++ project1/Program.cs
@@ -12,3 +12,21 @@ namespace project1
         }
     }
 }
+class Player //Making a player class for the players
+{
+	public string FirstName { get; set; } //first name method
+	public string LastName { get; set; } //last name method
+	public string Institution { get; set; } //institution method
+	public string Position { get; set; } //position method
+	public double Salary { get; set; } //salary method
+
+	//Making a constructor
+	public Player(string FirstName, string LastName, string Institution, string Position, double Salary)
+	{
+		this.FirstName = FirstName;
+		this.LastName = LastName;
+		this.Institution = Institution;
+		this.Position = Position;
+		this.Salary = Salary;
+	}
+}
